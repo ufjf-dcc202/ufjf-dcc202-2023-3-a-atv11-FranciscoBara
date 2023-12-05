b26e4181ff6b1a1edd56b669db29b0d3e7d46173
Francisco Bara <francisco.bara@estudante.ufjf.br>
2023-12-05 00:25:29 -0300

    achei os erros, concertei e passou nos testes

---
+++ main.js
@@ -0,0 +1,35 @@
+import { getLista, limpaLista, adicionaNaLista} from "./src/lista.js"
+
+const olItens = document.querySelector('#itens');
+const pEntrada = document.querySelector('#entrada');
+const btnAdicionar = document.querySelector('#adicionar');
+const btnLimpar = document.querySelector('#limpar');
+
+atualizarLista();
+
+function atualizarLista(){
+    olItens.innerHTML = "";
+    let lista = getLista();
+    for(let i = 0; i < lista.length; i++){
+        const li = document.createElement('li');
+        li.textContent = lista[i];
+        olItens.appendChild(li);
+    }
+}
+
+btnLimpar.addEventListener('click', limparItensDeLista);
+
+function limparItensDeLista(){
+    limpaLista();
+    atualizarLista();
+}
+
+btnAdicionar.addEventListener('click', criaElementoNaLista)
+
+function criaElementoNaLista(){
+    const texto = pEntrada.textContent
+    adicionaNaLista(texto)
+    atualizarLista()
+    pEntrada.textContent=""
+    pEntrada.focus()
+}
--- src/lista.js
+++ src/lista.js
@@ -1,17 +1,13 @@
 const lista = ["Um", "Dois", "Três"];
 
 export function getLista(){
-  return lista;
+    return structuredClone(lista);
 }
 
 export function limpaLista(){
-  lista.splice(0);
+    lista.splice(0);
 }
 
 export function adicionaNaLista(novoItem){
-  lista.push(novoItem);
-}
-
-export function removeDaLista(posicao){
-  lista.splice(posicao, posicao);
+    lista.push(novoItem);
 }
